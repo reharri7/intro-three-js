aee4736a72a5e7ae5d51de1faae595f7f72a31d5
Rhett Harrison <reharri7@asu.edu>
2023-10-07 22:23:57 -0700

    Adding anti-aliasing and resizing

--- main.js
+++ main.js
@@ -15,15 +15,23 @@ scene.add(cubeMesh);
 
 // init camera
 // const camera = new THREE.OrthographicCamera(-1, 1,1 , -1, 0.1, 200);
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 // init renderer
 const canvas = document.querySelector('canvas.threejs');
 const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(window.innerWidth, window.innerHeight);
+const maxPixelRatio = Math.min(window.devicePixelRatio, 2);
+renderer.setPixelRatio(maxPixelRatio);
+
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.autoRotate = true;
-renderer.setSize(window.innerWidth, window.innerHeight);
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
 const renderLoop = () => {
     controls.update();
     renderer.render(scene, camera);
